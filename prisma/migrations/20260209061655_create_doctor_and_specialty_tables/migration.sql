9201748dd77c452ec9a070cfd37a7f628ebdfc3b
lubanrahat <lubanrahat.dev@gmail.com>
2026-02-09 12:17:41 +0600

    Create Doctor entity, establish relationships with User and Specialty, and add Gender enum.

---
+++ prisma/migrations/20260209061655_create_doctor_and_specialty_tables/migration.sql
@@ -0,0 +1,68 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "doctor" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profilePhoto" TEXT,
+    "contactNumber" TEXT,
+    "address" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "registrationNumber" TEXT NOT NULL,
+    "expertise" INTEGER NOT NULL DEFAULT 0,
+    "gender" "Gender" NOT NULL,
+    "appoitmentFees" DOUBLE PRECISION NOT NULL,
+    "qualification" TEXT NOT NULL,
+    "currentWorkplace" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "averageRating" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "doctor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_specialty" (
+    "id" TEXT NOT NULL,
+    "doctorId" TEXT NOT NULL,
+    "specialtyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctor_specialty_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_email_key" ON "doctor"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_registrationNumber_key" ON "doctor"("registrationNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_userId_key" ON "doctor"("userId");
+
+-- CreateIndex
+CREATE INDEX "Doctor_email_idx" ON "doctor"("email");
+
+-- CreateIndex
+CREATE INDEX "Doctor_isDeleted_idx" ON "doctor"("isDeleted");
+
+-- CreateIndex
+CREATE INDEX "doctor_specialty_specialty_id_idx" ON "doctor_specialty"("specialtyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_specialty_doctorId_specialtyId_key" ON "doctor_specialty"("doctorId", "specialtyId");
+
+-- AddForeignKey
+ALTER TABLE "doctor" ADD CONSTRAINT "doctor_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialty" ADD CONSTRAINT "doctor_specialty_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialty" ADD CONSTRAINT "doctor_specialty_specialtyId_fkey" FOREIGN KEY ("specialtyId") REFERENCES "specialty"("id") ON DELETE CASCADE ON UPDATE CASCADE;
